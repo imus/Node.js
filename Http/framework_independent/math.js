ab49e23189411971c0391e7804c7acd4afb42ccc
sunsh <songhaosun@gmail.com>
2020-01-10 16:23:58 +0800

    asyncFibonacci

--- Http/framework_independent/math.js
+++ Http/framework_independent/math.js
@@ -16,6 +16,8 @@ factorial(4, 1)
 =24
 */
 
+// 数据密集型运算， n = 50, 阻止node事件循环，阻塞node线程。
+// 要么重构、要么创建一个后台服务
 // 0 1 1 2 3 5 8 13
 var fibonacci = exports.fibonacci = function (n) {
   if (n === 1) {
@@ -24,4 +26,22 @@ var fibonacci = exports.fibonacci = function (n) {
     return 1;
   }
   return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+// 计算过程交给事件循环调度，node进程会占用所有CPU负载，
+// 但是展示了通过  事件循环分派工作的技术  ,并不会阻塞服务器
+var fibonacciAsync = exports.fibonacciAsync = function (n, done) {
+  if (n === 1 || n === 2) {
+    return done(1);
+  } else {
+    process.nextTick(() => {
+      fibonacciAsync(n - 1, (val1) => {
+        process.nextTick(() => {
+          fibonacciAsync(n -2, (val2) => {
+            done(val1 + val2);
+          })
+        });
+      });
+    });
+  }
 }
